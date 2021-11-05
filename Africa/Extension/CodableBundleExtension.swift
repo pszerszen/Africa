a49504a0ccaf745e3f5746a8a53a0497d0b3d76a
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-05 15:06:35 +0100

    loading covers from names in json

---
+++ Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,28 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by Piotr Szerszeń on 05/11/2021.
+//
+
+import SwiftUI
+
+extension Bundle {
+
+    func decode<T: Codable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) from bundle.")
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+
+        return loaded
+    }
+}
---
+++ Africa/Model/CoverImage.swift
@@ -0,0 +1,14 @@
+//
+//  CoverImage.swift
+//  Africa
+//
+//  Created by Piotr Szerszeń on 05/11/2021.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+
+    let id: Int
+    let name: String
+}
