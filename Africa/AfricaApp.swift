f403194afa1a3612160811d0b8575d1c6d16f811
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-05 10:28:16 +0100

    initial commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Piotr Szerszeń on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
