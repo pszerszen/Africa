55b5929eef74757e7c16be18e69e14f991bcc455
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-08 15:59:27 +0100

    simple animal gallery

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct GalleryView: View {
+
+    @State private var selectedAnimal = "lion"
+
+    private let animals: [Animal] = Bundle.main.decode("animals.json")
+
+    let gridLayout: [GridItem] = Array.init(repeating: GridItem(.flexible()), count: 3)
+
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(Color.white, lineWidth: 8)
+                    }
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay {
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 1)
+                            }
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    }
+                }
+            }
+            .padding(.horizontal, 10.0)
+            .padding(.vertical, 50.0)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
