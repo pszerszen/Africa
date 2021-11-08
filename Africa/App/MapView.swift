8796d0cc821cb4acda23a4cbbe220d91cd94431e
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-08 13:48:44 +0100

    map with pinpointed national parks locations

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+
+    @State private var region: MKCoordinateRegion = {
+        let mapCoordinates =  CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        let mapZoomLevel = MKCoordinateSpan(latitudeDelta: 60.0, longitudeDelta: 60.0)
+
+        return MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+    }()
+
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+
     var body: some View {
-        Text("Map")
+        // MARK: - Basic map
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32.0, height: 32.0, alignment: .center)
+            }
+        }
     }
 }
 
--- Africa/Model/NationalParkLocation.swift
+++ Africa/Model/NationalParkLocation.swift
@@ -6,12 +6,17 @@
 //
 
 import Foundation
+import MapKit
 
-struct Location: Codable, Identifiable {
+struct NationalParkLocation: Codable, Identifiable {
 
     let id: String
     let name: String
     let image: String
     let latitude: Double
     let longitude: Double
+
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
 }
