e7f3276b72b3ed457b3be4f2edfa8332733ced64
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-08 14:45:08 +0100

    location indicator on map

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -22,14 +22,56 @@ struct MapView: View {
     var body: some View {
         // MARK: - Basic map
         Map(coordinateRegion: $region, annotationItems: locations) { item in
-//            MapMarker(coordinate: item.location, tint: .accentColor)
+            // Standard Markers
+            //            MapMarker(coordinate: item.location, tint: .accentColor)
+            // Custom Markers
+            //            MapAnnotation(coordinate: item.location) {
+            //                Image("logo")
+            //                    .resizable()
+            //                    .scaledToFit()
+            //                    .frame(width: 32.0, height: 32.0, alignment: .center)
+            //            }
+            //
+
             MapAnnotation(coordinate: item.location) {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 32.0, height: 32.0, alignment: .center)
+                MapAnnotationView(location: item)
             }
         }
+        .overlay(alignment: .top, content: {
+            HStack(alignment: .center, spacing: 12.0) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                VStack(alignment: .leading, spacing: 3.0) {
+                    HStack {
+                        Text("Latitude:")
+                            .fontWeight(.bold)
+                            .font(.footnote)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(Color.white)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Longitute:")
+                            .fontWeight(.bold)
+                            .font(.footnote)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(Color.white)
+                    }
+                }
+            }
+            .padding(.vertical, 12.0)
+            .padding(.horizontal, 16.0)
+            .background(Color.black.cornerRadius(8).opacity(0.6))
+            .padding()
+        })
     }
 }
 
