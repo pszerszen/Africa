50f5103ea4fb453f41c3fb0c6173461ed83901f9
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2021-11-08 14:16:44 +0100

    custom map marker

---
+++ Africa/View/MapAnnotationView.swift
@@ -0,0 +1,45 @@
+//
+//  MapAnnotationView.swift
+//  Africa
+//
+//  Created by Piotr Szerszeń on 08/11/2021.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+
+    let location: NationalParkLocation
+
+    @State private var animation = 0.0
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.accentColor)
+                .frame(width: 54.0, height: 54.0, alignment: .center)
+            Circle()
+                .stroke(Color.accentColor, lineWidth: 2)
+                .frame(width: 52, height: 52, alignment: .center)
+                .scaleEffect(CGFloat(animation) + 1)
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48.0, height: 48.0)
+            .clipShape(Circle())
+        }
+        .onAppear {
+            withAnimation(.easeOut(duration: 2).repeatForever(autoreverses: false)) {
+                animation = 1.0
+            }
+        }
+    }
+}
+
+struct MapAnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapAnnotationView(location: (Bundle.main.decode("locations.json") as [NationalParkLocation])[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
